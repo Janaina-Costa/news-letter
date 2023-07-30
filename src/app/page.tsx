93cd24ede0b315c5de52aa290a7a4d12e3f64ec9
Janaina-Costa <janainacosta.dev@gmail.com>
2023-07-30 07:04:53 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,35 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import style from "./page.module.css";
+import iconList from "../../public/icon-list.svg";
+import Image from "next/image";
+
+import { Form } from "@/components/Form";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={style.container}>
+      <div className={style.topImage}></div>
+      <div className={style.wrapper}>
+        <div className={style.wrapperTitle}>
+          <h1>Stay updated!</h1>
         </div>
+        <div className={style.wrapperSubTitle}>
+          <p>Join 60,000+ product managers receiving monthly updates on:</p>
+        </div>
+        <ul className={style.listContent}>
+          <li>
+            <Image src={iconList} alt="icone de lista" /> Product discovery and
+            building what matters
+          </li>
+          <li>
+            <Image src={iconList} alt="icone de lista" /> Measuring to ensure
+            updates are a success
+          </li>
+          <li>
+            <Image src={iconList} alt="icone de lista" /> And much more!
+          </li>
+        </ul>
+        <Form />
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
