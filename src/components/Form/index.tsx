f506092fe9ff9258d4e4f340dc8db40cfa21be21
Janaina-Costa <janainacosta.dev@gmail.com>
2023-07-30 23:03:30 -0300

    implementa estado do input

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,16 +1,34 @@
+"use client";
+
 import style from "./style.module.css";
 import { Input } from "./Input";
 import { Button } from "../Button";
+import { ChangeEvent, useState } from "react";
+
+interface IFormValues{
+  email: string;
+}
 
 export const Form = () => {
+  const [email, setEmail] = useState("");
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setEmail(value)      
+  };
+
   return (
     <form className={style.formField}>
       <Input
         type="email"
         placeholder="email@company.com"
         label="Email address"
+        value={email}
+        onchange={handleChange}
+        
+        
       />
-      <Button text="Subscrible to monthly newslatter" />
+      <Button text="Subscrible to monthly newslatter" type="submit" />
     </form>
   );
 };
